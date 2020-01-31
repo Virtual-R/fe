6b8c7fb8d163758f97dc4ccdd6b2ec8cdf39a475
Michelle <Michelle.Tinajero@outlook.com>
2020-01-30 20:09:58 -0600

    added to sign up form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Switch} from 'react-router-dom'
 import './App.css';  
+import SignUpForm from './components/SignUpForm';
 
 function App() {
   return (
     <Switch>
     <div className="App">
-     Virtual Reality App
+      <SignUpForm/>
     </div>
     </Switch>
   );
