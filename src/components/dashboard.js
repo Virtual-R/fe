90dd24e56603506904c2786be64f6d180b741122
Michelle <Michelle.Tinajero@outlook.com>
2020-02-09 21:51:58 -0600

    added more styling, more to go

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Container, Col, Row, CardImg } from 'reactstrap';
+import { Container, Col, Row } from 'reactstrap';
 import axios from 'axios';
 import DashboardList from "./dashboardList";
 import NavBar from "./NavBar"
 import "./dashboard.scss";
+import "./picvr.svg"
 
 const DashboardContainer = () => {
     const [project, setProject] = useState([])
@@ -18,16 +19,16 @@ const DashboardContainer = () => {
 
     return (
         <Container>
-        <NavBar />
-            <img src="vrpic.png"/>  
+        <NavBar /> 
+            <img src={require('./picvr.svg')}/>
             <h1 className="Header">DASHBOARD</h1>
-            <Row>
+            <Col  className="containerClass"> 
                 {project.map((item, id) => {
                     return (
-                        <Col> <DashboardList key={id} setProject={item} /> </Col>
+                        <Row className="projectClass"> <DashboardList key={id} setProject={item} /> </Row>
                     )
                 })}
-            </Row>
+            </Col>
         </Container>
     )
 }
